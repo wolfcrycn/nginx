75c6c892ef93752eb39ae5eabbcc21cc3d79c797
nelson.ma <nelson.ma@gsafer.com>
2019-12-26 17:10:12 +0800

    》update shell

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -4,7 +4,7 @@ echo 'The following Docker command build ngix image'
 set -x
 DATE=`date +%m%d%H%M `
 DIR="."
-sudo /usr/bin/docker build -t nginx_$DATE $DIR | tee $DIR/Docker_build_result.log
+docker build -t nginx_$DATE $DIR | tee $DIR/Docker_build_result.log
 RESULT=$(cat $DIR/Docker_build_result.log | tail -n 1)
 if [["$RESULT" != *Successfully*]];then
   exit -1
